55adeb7017765b1eb98e8ffbb06a000b358ca63f
Andrew_2212 <6612273@gmail.com>
2013-12-30 11:41:25 +0200

    Sorting interface is added

--- src/com/playstation/PlayStationStorage.java
+++ src/com/playstation/PlayStationStorage.java
@@ -1,6 +1,7 @@
 package com.playstation;
 
 import com.storage.StorageSorter;
+import com.sun.xml.internal.bind.v2.TODO;
 
 import java.util.Scanner;
 
@@ -12,11 +13,12 @@ import java.util.Scanner;
  * To change this template use File | Settings | File Templates.
  */
 public class PlayStationStorage {
-//    TODO interface for sorter and filter
 
     private static final int FIRST_CHAR = 0;
     private static final char N = 'N';
     private static final char V = 'V';
+    private static final char A = 'A';
+    private static final char D = 'D';
 
     public static void letSortStorage(){
         System.out.println("Press either " + N + " to sort by 'name' or " + V + " to sort by 'value'");
@@ -32,20 +34,81 @@ public class PlayStationStorage {
                 break;
             default:
                 Printer.printItIsInvalidEnter();
-
+              break;
         }
          PlayStation.play();
     }
 
+    public static void letFilterStorage(){
+        System.out.println("Press either " + N + " to filter by 'name' or " + V + " to filter by 'value'");
+        String enter = new Scanner(System.in).next().toUpperCase();
+
+        switch(enter.charAt(FIRST_CHAR)){
+
+            case N:
+                filterByName();
+                break;
+            case V:
+                filterByValue();
+                break;
+            default:
+                Printer.printItIsInvalidEnter();
+                break;
+        }
+        PlayStation.play();
+    }
+
+//    ======Private Methods===================
+
+//    ------Sorting-------------
+
     private static void sortByName(){
-        StorageSorter.sortByNameAscent();
+
+        System.out.println("To sort by ascent press " + A + ", to sort by descent press " + D);
+        String enter = new Scanner(System.in).next();
+
+        switch(enter.toUpperCase().charAt(FIRST_CHAR)){
+            case A:
+                StorageSorter.sortByNameAscent();
+                break;
+            case D:
+                StorageSorter.sortByNameDescent();
+                break;
+            default:
+                Printer.printItIsInvalidEnter();
+                break;
+        }
 
         Printer.printStorageByRequest();
     }
 
     private static void sortByValue(){
-        StorageSorter.sortByValueAscent();
+        System.out.println("To sort by ascent press " + A + ", to sort by descent press " + D);
+        String enter = new Scanner(System.in).next();
+
+        switch(enter.charAt(FIRST_CHAR)){
+            case A:
+                StorageSorter.sortByValueAscent();
+                break;
+            case D:
+                StorageSorter.sortByValueDescent();
+                break;
+            default:
+                Printer.printItIsInvalidEnter();
+                break;
+        }
 
         Printer.printStorageByRequest();
+    }
+
+//    ---------Filtering-----------
+
+
+    private static void filterByName() {
+//        TODO
+    }
+
+    private static void filterByValue() {
+//        TODO
     }
 }
